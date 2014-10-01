384cee344f2b76716fd9c3b53b48f1e93ec326ed
rbnsfr <rbnsfr@outlook.com>
2014-10-01 08:39:51 -0400

    Chapter 5

--- ChatAWhile/ChatAWhile/Program.cs
+++ ChatAWhile/ChatAWhile/Program.cs
@@ -10,24 +10,22 @@ namespace Chapter5
     {
         static void Main(string[] args)
         {
-            int[] areacodes     = { 262,  414,  608,  715,  815,  920  };
+            int[] areacodes = { 262, 414, 608, 715, 815, 920 };
             double[] perminrate = { 0.07, 0.10, 0.05, 0.16, 0.24, 0.14 };
-            bool isValidCode    = false;
+            bool isValidCode = false;
+            int b = 0;
             double total;
 
             Console.Write("Enter your area code: ");
-            for (int a = 0; a < areacodes.Length; ++a)
+            if (Console.ReadLine() == areacodes[b].ToString())
             {
-                if (Console.ReadLine() == areacodes[a].ToString())
-                {
-                    isValidCode = true;
-                    total = Convert.ToDouble(Console.ReadLine()) * perminrate[a];
-                }
+                isValidCode = true;
+                total = Convert.ToDouble(Console.ReadLine()) * perminrate[b];
             }
             if (isValidCode)
             {
                 Console.Write("Enter how many minutes your call lasted: ");
-                Console.WriteLine("\nYour total is {0}.", total);
+                Console.WriteLine("\nYour total is {0}.", perminrate[b]);
                 Thread.Sleep(3000);
             }
             else
---
+++ ChatAWhile/TeeBall/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeeBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] statistics = new int[12,5];
+
+            while (true)
+            {
+                Console.Write("Enter the player number (999 to quit): ");
+
+                if (Console.ReadLine() == "999") break;
+                else statistics[0, 0] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
